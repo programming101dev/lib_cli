e28e5a9284942757b36ab37dceca48a4f02b87b4
programming101dev <113087924+programming101dev@users.noreply.github.com>
2026-08-06 13:27:27 -0700

    Enforce semantic wrapper boundaries

--- src/getopt.c
+++ src/getopt.c
@@ -15,8 +15,6 @@
  */
 
 #include "p101_cli/p101_getopt.h"
-#include "p101_cli/p101_stdlib.h"
-#include "p101_cli/p101_unistd.h"
 #include <p101_env/wrapper.h>
 
 /*
--- src/stdlib.c
+++ src/stdlib.c
@@ -14,9 +14,7 @@
  * limitations under the License.
  */
 
-#include "p101_cli/p101_getopt.h"
 #include "p101_cli/p101_stdlib.h"
-#include "p101_cli/p101_unistd.h"
 #include <p101_env/wrapper.h>
 
 /*
